61a5b9b166fdeb8af676526191b373749528ab6a
AllanTeixeira <35341496+AllanTeixeira@users.noreply.github.com>
2018-09-23 22:10:40 -0300

    Add files via upload

--- routes/rotas.js
+++ routes/rotas.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 const controller = require('../controllers/Controller');
 
+//Controle das Rotas
 router.get('/', controller.init);
 router.post('/add', controller.post);
 router.get('/list', controller.get);
